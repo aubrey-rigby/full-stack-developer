1ae4c0981c9ac1da1a51a7da4ef1e9cfd752a988
aubrey-heim <aubrey.l.heim@gmail.com>
2021-02-01 20:25:38 -0800

    styled contact info

--- src/components/ContactBox/index.js
+++ src/components/ContactBox/index.js
@@ -9,8 +9,11 @@ function ContactBox(props) {
             <div className="info-box mt-3">
                 <img className="float-left mb-2" id="headshot" width="200" src={props.image} alt="Aubrey Heim"/>
                 <h2>Phone:</h2>
+                <br />
                 <a  href={`tel:${props.phone}`}>{props.phone}</a>
+                <br />
                 <h2 className="mt-3">Email:</h2>
+                <br />
                 <a href={`mailto:${props.email}`}>{props.email}</a>
                 <a href={`https://github.com/${props.github}`} rel="noreferrer" target="_blank"><button className="mt-2 contact-button btn"><i className="fab fa-github mr-3"></i>GitHub Profile</button></a>
                 <a href="https://www.linkedin.com/in/aubreyheim/" rel="noreferrer" target="_blank"><button className="mt-2 contact-button btn"><i className="fab fa-linkedin mr-3"></i>LinkedIn Profile</button></a>
